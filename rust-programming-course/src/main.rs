f6b14ad93426c9ec96ddfd215bea9b764780e6a6
Lenovo <longdieu12x@gmail.com>
2024-02-12 01:12:41 +0700

    add module

--- rust-programming-course/src/main.rs
+++ rust-programming-course/src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+    // tuples
+    let simple_tuples = (String::from("This is x"), "hello", 5);
+    println!("This is tuple {}", simple_tuples.1);
+
+    // loop
+    'outer: loop {
+        let mut i = 0;
+        'inner: loop {
+            i = i + 1;
+            if i == 3 {
+                println!("Touch");
+                break 'inner;
+            }
+            if i > 5 {
+                println!("Break outer loop");
+                break 'outer;
+            }    
+        }
+        println!("Break from inner loop");
+        break;
+    }
+
+    // matches
+    let proposal_status = false;
+    let proposal_name;
+
+    match proposal_status {
+        true => proposal_name = "Ok good",
+        false => proposal_name = "Damn so bad",
+    }
+
+    println!("proposal name: {proposal_name}");
+
+    // range inclusive
+    let vecs = vec![1,2,3];
+    let small_vecs = vecs[1..=2].to_vec();
+    println!("Vector: {:?}", small_vecs);
 }
---
+++ rust-programming-course/target/debug/deps/rust_programming_course-fccae1b1e87a4e9f.d
@@ -0,0 +1,5 @@
+/media/lenovo/DATABOX/Developer/learn-rust/rust-programming-course/target/debug/deps/rust_programming_course-fccae1b1e87a4e9f: src/main.rs
+
+/media/lenovo/DATABOX/Developer/learn-rust/rust-programming-course/target/debug/deps/rust_programming_course-fccae1b1e87a4e9f.d: src/main.rs
+
+src/main.rs:
