2e77f0bf1e49cf9f9186b37369ebfc0d7f38c94d
Lenovo <longdieu12x@gmail.com>
2024-03-02 18:23:16 +0700

    finish section 9

--- rust-programming-course/1 - Flexibility and Abstraction with Generics and Traits/src/main.rs
+++ rust-programming-course/1 - Flexibility and Abstraction with Generics and Traits/src/main.rs
@@ -1,3 +1,75 @@
+// ----------------------------------------------
+// 		Associated Types in Traits
+// ----------------------------------------------
+
+#[derive(Debug)]
+struct Km {
+    value: u32,
+}
+
+#[derive(Debug)]
+struct Kmh {
+    value: u32,
+}
+
+#[derive(Debug)]
+struct Miles {
+    value: u32,
+}
+
+#[derive(Debug)]
+struct Mph {
+    value: u32,
+}
+
+// impl Kmh {
+//     fn distance_in_three_hours(&self) -> Km {
+//         Km {
+//             value: self.value * 3,
+//         }
+//     }
+// }
+
+// impl Mph {
+//     fn distance_in_three_hours(&self) -> Miles {
+//         Miles {
+//             value: self.value * 3,
+//         }
+//     }
+// }
+
+trait DistanceThreeHours<Distance> {
+    fn distance_in_three_hours(&self) -> Distance;
+}
+
+impl DistanceThreeHours<Km> for Kmh {
+    fn distance_in_three_hours(&self) -> Km {
+        Km {
+            value: self.value * 3,
+        }
+    }
+}
+
+impl DistanceThreeHours<Miles> for Mph {
+    fn distance_in_three_hours(&self) -> Miles {
+        Miles {
+            value: self.value * 3,
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let speed_Kmh = Kmh { value: 90 };
+    let distance_Km = speed_Kmh.distance_in_three_hours();
+
+    println!(
+        "At {:?}, you will travel {:?} in 3 hours",
+        speed_Kmh, distance_Km
+    );
+
+    let speed_Mph = Mph { value: 90 };
+    let distance_Miles = speed_Mph.distance_in_three_hours();
+    println!(
+        "At {:?}, you will travel {:?}, in 3 hours",
+        speed_Mph, distance_Miles
+    );
 }
