110c8661e17ba9a233e47c9a2b2faeb47a489c9a
Lenovo <longdieu12x@gmail.com>
2024-03-02 15:43:39 +0700

    remove redundant

--- rust-programming-course/target/debug/deps/rust_programming_course-048fd15244d14931.d
+++
@@ -1,5 +0,0 @@
-/media/lenovo/DATABOX/Developer/learn-rust/rust-programming-course/target/debug/deps/librust_programming_course-048fd15244d14931.rmeta: src/main.rs
-
-/media/lenovo/DATABOX/Developer/learn-rust/rust-programming-course/target/debug/deps/rust_programming_course-048fd15244d14931.d: src/main.rs
-
-src/main.rs:
--- rust-programming-course/target/debug/deps/rust_programming_course-2a45ded80070d52a.d
+++
@@ -1,5 +0,0 @@
-/media/lenovo/DATABOX/Developer/learn-rust/rust-programming-course/target/debug/deps/librust_programming_course-2a45ded80070d52a.rmeta: src/main.rs
-
-/media/lenovo/DATABOX/Developer/learn-rust/rust-programming-course/target/debug/deps/rust_programming_course-2a45ded80070d52a.d: src/main.rs
-
-src/main.rs:
