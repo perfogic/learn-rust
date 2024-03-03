409c9ee7f3fdfc936dbca4c029d64eb74f05e153
perfogic <longdieu12x@gmail.com>
2024-03-03 16:29:45 +0700

    add solution for doubly linekd list problem 2

--- rust-programming-course/4 - Impelmenting Typical Data Structures/src/main.rs
+++ rust-programming-course/4 - Impelmenting Typical Data Structures/src/main.rs
@@ -1,5 +1,5 @@
-//Problem 1: Add the method add_back() to the implementation of doubly linked list below. 
-// The method should add the element at the back or at the end of the linked list.    
+//Problem 2: Add the method remove_back() to the implementation of doubly linked list below.
+// The method should remove the element at the back or at the end of the linked list.
 
 use std::{cell::RefCell, rc::Rc};
 #[derive(Debug)]
@@ -43,29 +43,27 @@ impl Doubly_Linklist {
     }
 
     fn add_back(&mut self, element: i32) {
-        /* Put your Code Here*/
         let new_tail = Node::new(element);
-
         match self.tail.take() {
             Some(old_tail) => {
                 old_tail.borrow_mut().next = Some(new_tail.clone());
                 new_tail.borrow_mut().prev = Some(old_tail.clone());
                 self.tail = Some(new_tail);
-            },
+            }
             None => {
-                self.tail = Some(new_tail.clone());
-                self.head = Some(new_tail);
-            },
+                self.head = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
         }
     }
-    
+
     fn remove(&mut self) -> Option<i32> {
         if self.head.is_none() {
             println!("List is empty so we can not remove");
             None
         } else {
             let removed_val = self.head.as_ref().unwrap().borrow().element;
-            self.head
+            let _ = self.head
                 .take()
                 .map(|old_head| match old_head.borrow_mut().next.take() {
                     Some(new_head) => {
@@ -80,6 +78,28 @@ impl Doubly_Linklist {
                     }
                 });
             Some(removed_val)
+        }
+    }
+
+    fn remove_back(&mut self) {
+        /*Add Code Here */
+        if self.tail.is_none() {
+            println!("List is empty");
+        } else {
+            let _ = self.tail.take().map(|old_tail| {
+                match old_tail.borrow_mut().prev.take() {
+                    Some(previous_tail) => {
+                        previous_tail.borrow_mut().next = None;
+                        self.tail = Some(previous_tail.clone());
+                        self.tail.clone()
+                    },
+                    None => {
+                        self.head = None;
+                        self.tail = None;
+                        None
+                    },
+                }
+            });
         }
     }
 
@@ -109,7 +129,15 @@ fn main() {
     list1.add(34);
     list1.add(36);
     list1.print();
-    list1.add_back(1);
-    list1.add_back(2);
+    println!("After removing the elements from the back/end");
+    list1.remove_back();
+    list1.remove_back();
     list1.print();
+
+    let mut x = Some(2);
+    let y = x.take().map(|val| {
+        x = Some(val + 3);
+        2
+    });
+    println!("{:?} {:?}", x, y);
 }
