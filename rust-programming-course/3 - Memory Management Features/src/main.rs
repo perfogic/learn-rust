ce5860a2cc72a379e9c822c58801f22ad1dbbaff
perfogic <longdieu12x@gmail.com>
2024-03-03 14:31:44 +0700

    finish section Refcell

--- rust-programming-course/3 - Memory Management Features/src/main.rs
+++ rust-programming-course/3 - Memory Management Features/src/main.rs
@@ -1,23 +1,58 @@
-use std::rc::Rc;
-enum List {
-    Cons(i32, Option<Rc<List>>),
-}
+// -------------------------------------------
+// 		RefCell Smart Pointer
+// -------------------------------------------
+
+use std::{cell::RefCell, rc::Rc};
+
 fn main() {
     /**
-     * Here, both a,b,c are owner of the data List::Cons(1, Some(Rc::new(List::Cons(2, None))))
-     * Which is done by using Rc smart pointer, the Rc::clone is not clone a deep copy of
-     * the data like other clone, it just add a reference count += 1 (which is really effienct),
-     * strong_count() here show all the reference count which will illustrate you about this
-     * concept. Author: Perfogic!
+     * With the RefCell, you can both borrow borrow_mut and borrow_immutatble at the same time
+     * Even you make the collision range between mutable, immutable, the compiler does not show
+     * the error, but it will throw an error at the runtime. This will be so useful in doubly linkedlist,
+     * and great to combine with Rc, to have multiple reference and each have a mutable capability
      */
-    let a = Rc::new(List::Cons(1, Some(Rc::new(List::Cons(2, None)))));
-    println!("Reference count after a: {}", Rc::strong_count(&a));
-    {
-        let b = List::Cons(3, Some(Rc::clone(&a)));
-        println!("Reference count after b: {}", Rc::strong_count(&a));
+    // Example 1: Borrowing rules at run time
+    // let mut x = 50;
+    // let x1 = &x;
+    // let x2 = &x;
+    // let x3 = &mut x;
 
-        let c = List::Cons(4, Some(Rc::clone(&a)));
-        println!("Reference count after c: {}", Rc::strong_count(&a));
-    }
-    println!("Reference count after scope: {}", Rc::strong_count(&a));
+    // println!("{} {} ", x1, x2);
+
+    let a = RefCell::new(10);
+
+    //{ // add the scope later on
+    let b = a.borrow();
+    let c = a.borrow();
+    //}
+
+    drop(b); // add later on, remove after adding the scope above
+    drop(c); // add later on
+    let d = a.borrow_mut();
+    // drop(d) // add later on
+    //println!("{} {}", b, c); // later on delete this
+    //println!("Value of a is : {:?}", a); // add later on
+
+
+    // Example 2: Interior mutability
+    // let x = 32;
+    // let x1 = &mut x;
+
+    let a = RefCell::new(10);
+    //let c = *a; // add later on
+    let mut b = a.borrow_mut();
+    *b = 15;
+
+    drop(b); // add later on
+    // If the drop(b) is commented, then we can't not see the value a
+    // because it is already borrowed
+    println!("{:?}", a);
+
+    // Example 3 
+    let a = Rc::new(RefCell::new(String::from("c++")));
+    let b = Rc::clone(&a);
+    // With this case, because we have multiple reference
+    // so we can still see the value of a
+    *b.borrow_mut() = String::from("rust");
+    println!("{:?}", a);
 }
