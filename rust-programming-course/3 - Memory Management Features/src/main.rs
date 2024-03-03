7ef16c9715761484d825926d6e6510ffbff825e5
perfogic <longdieu12x@gmail.com>
2024-03-03 14:09:00 +0700

    finish RC smart pointer, skip Box because so easy

--- rust-programming-course/3 - Memory Management Features/src/main.rs
+++ rust-programming-course/3 - Memory Management Features/src/main.rs
@@ -1,24 +1,16 @@
-#[derive(Debug)]
-struct ArrayProcessor<'a> {
-    data: &'a [i32],
+use std::rc::Rc;
+enum List {
+    Cons(i32, Option<Rc<List>>),
 }
-
-impl<'a> ArrayProcessor<'a> {
-    fn update_data(&mut self, arr: &'a [i32]) -> &[i32] {
-        let previous_data = self.data;
-        self.data = arr;
-        previous_data
-    }
-
-}
-
 fn main() {
-    let mut some_data = ArrayProcessor { data: &[4, 5, 6] };
-    let curious_data = &[5, 6, 7];
-
+    let a = Rc::new(List::Cons(1, Some(Rc::new(List::Cons(2, None)))));
+    println!("Reference count after a: {}", Rc::strong_count(&a));
     {
-        let prev_data = some_data.update_data(curious_data);
-        println!("Prev data: {:?}", prev_data);
-        println!("Some data: {:?}", some_data);
+        let b = List::Cons(3, Some(Rc::clone(&a)));
+        println!("Reference count after b: {}", Rc::strong_count(&a));
+
+        let c = List::Cons(4, Some(Rc::clone(&a)));
+        println!("Reference count after c: {}", Rc::strong_count(&a));
     }
+    println!("Reference count after scope: {}", Rc::strong_count(&a));
 }
