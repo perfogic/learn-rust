118f53916060682c8d46a40de3eb863d03612b66
perfogic <longdieu12x@gmail.com>
2024-03-03 16:01:30 +0700

    update code for doubly linked list

--- rust-programming-course/4 - Impelmenting Typical Data Structures/src/main.rs
+++ rust-programming-course/4 - Impelmenting Typical Data Structures/src/main.rs
@@ -1,69 +1,127 @@
-//Problem 2: We want to change the linked list implementation by making the element part of 
-// the node as generic rather then concrete i32. Make approperiate changes to the code below. 
-// For printing, T should have the trait bound of  std::fmt::Debug and 
-// for the peek to work, T must also have the trait bound of std::marker::Copy  
+// -------------------------------------------
+// 		Doubly Link List (Part 2)
+// -------------------------------------------
 
-use std::fmt::Debug;
-
-
+use std::{cell::RefCell, rc::Rc};
 #[derive(Debug)]
-struct Linklist<T> {  // This line needs a fix 
-    head: pointer<T>, // This line needs a fix 
+struct Doubly_Linklist {
+    head: pointer,
+    tail: pointer,
 }
 
 #[derive(Debug)]
-struct Node<T> {
-    element: T,
-    next: pointer<T>,  // This line needs a fix  
+struct Node {
+    element: i32,
+    next: pointer,
+    prev: pointer,
 }
-type pointer<T> = Option<Box<Node<T>>>;  // This line needs a fix 
 
-impl<T: Debug + Copy> Linklist<T> {  // This line needs a fix 
-    fn new() -> Linklist<T> { // This line needs a fix 
-        Linklist { head: None }
-    }
+type pointer = Option<Rc<RefCell<Node>>>;
 
-    fn add(&mut self, element: T) { // This line needs a fix 
-        let previous_head = self.head.take();
-        let new_head = Some(Box::new(Node {
-            element: element,
-            next: previous_head,
-        }));
-        self.head = new_head;
-    }
-
-    fn remove(&mut self) -> Option<T> {  // This line needs a fix 
-        match self.head.take() {
-            Some(previous_head) => {
-                self.head = previous_head.next;
-                Some(previous_head.element)
-            }
-            None => None,
+impl Doubly_Linklist {
+    fn new() -> Self {
+        Doubly_Linklist {
+            head: None,
+            tail: None,
         }
     }
 
-    fn peek(&self) -> Option<T> {  // This line needs a fix 
-        match &self.head {
-            Some(H) => Some(H.element),
-            None => None,
+    fn add(&mut self, element: i32) {
+        let new_head = Node::new(element);
+
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head.clone());
+                self.head = Some(new_head);
+            }
+
+            None => {
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            }
+        }
+    }
+
+    // Case: 1
+    // -----------------------
+    //         Head        Tail
+    // None <-- 1 --> 2 --> 3 --> None
+    // None     1 <-- 2 <-- 3     None
+    // -----------------------
+
+    // Case: 1 (After Removal)
+    // -----------------------
+    //       Head  Tail
+    // None <-- 2 --> 3 --> None
+    // None     2 <-- 3     None
+    // -----------------------
+
+    // Case: 2
+    // -----------------------
+    //       Head
+    //       Tail
+    // None <-- 1 --> None
+    // -----------------------
+
+    // Case: 2 (After Removal)
+    // -----------------------
+    //       Head = None
+    //       Tail = None
+    // -----------------------
+
+    fn remove(&mut self) -> Option<i32> {
+        if self.head.is_none() {
+            println!("List is empty so we can not remove");
+            None
+        } else {
+            let removed_val = self.head.as_ref().unwrap().borrow().element;
+            self.head
+                .take()
+                .map(|old_head| match old_head.borrow_mut().next.take() {
+                    Some(new_head) => {
+                        new_head.borrow_mut().prev = None;
+                        self.head = Some(new_head);
+                        self.head.clone()
+                    }
+                    None => {
+                        self.tail = None;
+                        println!("List is empty after removal");
+                        None
+                    }
+                });
+            Some(removed_val)
         }
     }
 
     fn print(&self) {
-        let mut list_traversal = &self.head;
-        while !list_traversal.is_none() {
-            println!("{:?}", list_traversal.as_ref().unwrap().element);
-            list_traversal = &list_traversal.as_ref().unwrap().next;
+        let mut traversal = self.head.clone();
+        while !traversal.is_none() {
+            println!("{}", traversal.as_ref().unwrap().borrow().element);
+            traversal = traversal.unwrap().borrow().next.clone();
         }
     }
 }
-fn main() {
-    let mut list = Linklist::new();
-    list.add(5);
-    list.add(7);
-    list.add(10);
-    list.add(15);
-    list.add(20);
 
-    println!("{:?}", list.peek());
+impl Node {
+    fn new(element: i32) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(Node {
+            element: element,
+            next: None,
+            prev: None,
+        }))
+    }
+}
+fn main() {
+    let mut list1 = Doubly_Linklist::new();
+
+    list1.add(30);
+    list1.add(32);
+    list1.add(34);
+    list1.add(36);
+    list1.print();
+
+    list1.remove();
+    println!("After Removal");
+    list1.print();
 }
