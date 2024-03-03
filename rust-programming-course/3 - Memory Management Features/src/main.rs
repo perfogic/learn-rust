b8dd2b3ae4694a312baba717049d4b1acd3a2bf7
perfogic <longdieu12x@gmail.com>
2024-03-03 14:11:43 +0700

    add comment about Rc Smart pointer

--- rust-programming-course/3 - Memory Management Features/src/main.rs
+++ rust-programming-course/3 - Memory Management Features/src/main.rs
@@ -3,6 +3,13 @@ enum List {
     Cons(i32, Option<Rc<List>>),
 }
 fn main() {
+    /**
+     * Here, both a,b,c are owner of the data List::Cons(1, Some(Rc::new(List::Cons(2, None))))
+     * Which is done by using Rc smart pointer, the Rc::clone is not clone a deep copy of
+     * the data like other clone, it just add a reference count += 1 (which is really effienct),
+     * strong_count() here show all the reference count which will illustrate you about this
+     * concept. Author: Perfogic!
+     */
     let a = Rc::new(List::Cons(1, Some(Rc::new(List::Cons(2, None)))));
     println!("Reference count after a: {}", Rc::strong_count(&a));
     {
